d0890c1e20f948795ad47fd53f779dfdbb7d3be3
cowling <cowcowling@gmail.com>
2015-06-20 00:21:14 -0400

    first commit

---
+++ src/GameTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+
+public class GameTest {
+
+//	@Test
+//	public void testMoveNorth() {
+//		//precondition
+//		Player p = mock(Player.class);
+//		House h = mock(House.class);
+//		//stubbing (no need to stubbing when there is no return value, but you can verify them)
+//		//when(h.moveNorth());
+//		Game g = new Game(p, h);
+//		
+//		//execution
+//		int result = g.doSomething("N");
+//		//Assertion
+//		verify(h).moveNorth();
+//		verify(h, times(1)).moveNorth();
+//		verify(h, never()).moveSouth();
+//		assertEquals(result, 0);
+//		
+//	}
+
+}
